91ad69ea13ec5589fceb7157fb3469b969d987f7
dima <tipanswer@gmail.com>
2024-07-05 12:34:25 +0300

    Remove alias @/

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+
 import * as path from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,6 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'),
             shared: path.resolve(__dirname, './src/shared'),
             app: path.resolve(__dirname, './src/app'),
             pages: path.resolve(__dirname, './src/pages'),
